d8e8644ae8941a6d7501aa3a4af2ce8094291834
Anton <hamsteramk2017@gmail.com>
2026-02-27 15:55:15 +0300

    version_2

--- src/Lab1/Entities/IRouteSection.cs
+++
@@ -1,8 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab1.ResultTypes;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.Entities;
-
-public interface IRouteSection
-{
-    MoveResult Move(Train train);
-}
